2c2a94805f0cd5bdde6d5c816511e036c871b9ea
henry <henryboy753951@gmail.com>
2023-12-17 18:43:53 +0800

    modified:   stages/__init__.py

--- stages/__init__.py
+++ stages/__init__.py
@@ -2,7 +2,7 @@ from .IFStage import IFStage
 from .IDStage import IDStage
 from .EXStage import EXStage
 from .MEMStage import MEMStage
-# from WBStage import WBStage
+from .WBStage import WBStage
 from .ControlUnit import ControlUnit
 
 __all__ = ["EXStage", "IDStage", "IFStage", "MEMStage", "WBStage", "ControlUnit"]
